1851558399ae0a18cfade5d04b9b67fda2c4ca12
Simon B. <nox@avox.at>
2023-12-05 14:21:05 +0100

    Added sleep

--- generator.c
+++ generator.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <ctype.h>
+#include <limits.h>
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Logging
+
+static void printStderrAndExit(const char *output, ...) {
+    va_list args;
+    va_start(args, output);
+    vfprintf(stderr, output, args);
+    va_end(args);
+    exit(EXIT_FAILURE);
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Util
+
+static void printUsageAndExit(void) {
+    printStderrAndExit("Usage: %s EDGE1...\n", PROGRAM_NAME);
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Main
 
 int main(int argc, char **argv) {
     printf("Hello generator\n");
