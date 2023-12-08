a66a27d1dac47e398eb0e9e702d753ff868e9069
Simon B <nox@avox.at>
2023-12-08 13:13:08 +0100

    Made supervisor cleanup save

--- supervisor.c
+++ supervisor.c
@@ -57,6 +57,15 @@ volatile sig_atomic_t quitSignalRecieved = false;
 // NAME OF CONSTANTS IN UPPERCASE
 // Use meaningful variable and constant names
 
+circular_buffer_data_t *circularBufferData = NULL;
+semaphore_colleciton_t semaphoreCollection = {
+    NULL,
+    NULL,
+    NULL,
+};
+
+static void cleanup();
+
 // ---------------------------------------------------------------------------------------------------------------------
 // Logging
 
@@ -68,11 +77,12 @@ volatile sig_atomic_t quitSignalRecieved = false;
  * @param output Formatted output string
  * @param ... Fomat elements
  */
-static void printStderrAndExit(const char *output, ...) {
+static void printStderrCleaupAndExit(const char *output, ...) {
     va_list args;
     va_start(args, output);
     vfprintf(stderr, output, args);
     va_end(args);
+    cleanup();
     exit(EXIT_FAILURE);
 }
 
@@ -86,7 +96,7 @@ static void printStderrAndExit(const char *output, ...) {
  * @details global variables: PROGRAM_NAME 
  */
 static void printUsageAndExit(void) {
-    printStderrAndExit("Usage: %s [-n limit] [-w delay] [-p]\n", PROGRAM_NAME);
+    printStderrCleaupAndExit("Usage: %s [-n limit] [-w delay] [-p]\n", PROGRAM_NAME);
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
@@ -121,11 +131,12 @@ static program_parameters_t parseArguments(int argc, char **argv) {
                 programParameters.limit = strtol(optarg, &endptr1, 10);
                 if(programParameters.limit == LONG_MIN || programParameters.limit == LONG_MAX) {
                     if(errno == ERANGE) {
-                        printStderrAndExit("[%s] ERROR: Converting integer failed: %s\n", PROGRAM_NAME, strerror(errno));
+                        printStderrCleaupAndExit("[%s] ERROR: Converting integer failed: %s\n", PROGRAM_NAME, strerror(errno));
                     }
                 }
                 if (endptr1 == optarg) {
-                    printStderrAndExit("[%s] ERROR: No digits were found in the input string for limit!\n", PROGRAM_NAME);
+                    fprintf(stderr, "[%s] ERROR: No digits were found in the input string for limit!\n", PROGRAM_NAME);
+                    printUsageAndExit();
                 }
                 if(programParameters.limit < 0) {
                     fprintf(stderr, "[%s] ERROR: Limit cannot be smaller than 0!\n", PROGRAM_NAME);
@@ -141,11 +152,12 @@ static program_parameters_t parseArguments(int argc, char **argv) {
                 programParameters.delay = strtol(optarg, &endptr2, 10);
                 if(programParameters.delay == LONG_MIN || programParameters.delay == LONG_MAX) {
                     if(errno == ERANGE) {
-                        printStderrAndExit("[%s] ERROR: Converting integer failed: %s\n", PROGRAM_NAME, strerror(errno));
+                        printStderrCleaupAndExit("[%s] ERROR: Converting integer failed: %s\n", PROGRAM_NAME, strerror(errno));
                     }
                 }
                 if (endptr2 == optarg) {
-                    printStderrAndExit("[%s] ERROR: No digits were found in the input string for wait!\n", PROGRAM_NAME);
+                    fprintf(stderr, "[%s] ERROR: No digits were found in the input string for wait!\n", PROGRAM_NAME);
+                    printUsageAndExit();
                 }
                 if(programParameters.delay < 0) {
                     fprintf(stderr, "[%s] ERROR: Delay cannot be smaller than 0!\n", PROGRAM_NAME);
@@ -165,7 +177,7 @@ static program_parameters_t parseArguments(int argc, char **argv) {
                 break;
             case '?':
             default:
-                fprintf(stderr, "[%s] ERROR: Unkn#include <semaphore.h>own option: -%c\n", PROGRAM_NAME, optopt);
+                fprintf(stderr, "[%s] ERROR: Unknown option: -%c\n", PROGRAM_NAME, optopt);
                 printUsageAndExit();
                 break;
         }
@@ -188,18 +200,25 @@ static program_parameters_t parseArguments(int argc, char **argv) {
  * 
  * @param circularBufferData pointer to the mapped shared memory circular buffer
  */
-static void closeSHM(circular_buffer_data_t* circularBufferData) {
+static int closeSHM() {
+    int returnValue = 0;
+
     if(circularBufferData != NULL) {
         if(munmap(circularBufferData, sizeof(circular_buffer_data_t)) == -1) {
-            printStderrAndExit("[%s] ERROR: Failed to unmap shared memory: %s\n", PROGRAM_NAME, strerror(errno));
+            fprintf(stderr, "[%s] ERROR: Failed to unmap shared memory: %s\n", PROGRAM_NAME, strerror(errno));
+            returnValue = -1;
         }
+        circularBufferData = NULL;
     }
     
     if(shm_unlink(SHM_NAME) == -1) {
         if(errno != ENOENT) {
-            printStderrAndExit("[%s] ERROR: Failed to unlink shared memory: %s\n", PROGRAM_NAME, strerror(errno));
+            fprintf(stderr, "[%s] ERROR: Failed to unlink shared memory: %s\n", PROGRAM_NAME, strerror(errno));
+            returnValue = -1;
         }
     }
+
+    return returnValue;
 }
 
 /**
@@ -210,30 +229,28 @@ static void closeSHM(circular_buffer_data_t* circularBufferData) {
  * 
  * @return A pointer to the mapped shared memory circular buffer
  */
-static circular_buffer_data_t* openSHM(void) {
+static void openSHM(void) {
+    if(circularBufferData != NULL) {
+        return;
+    }
+
     int sharedMemoryFd;
     if((sharedMemoryFd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0600)) == -1) {
-        printStderrAndExit("[%s] ERROR: Failed to open shared memory: %s\n", PROGRAM_NAME, strerror(errno));
+        printStderrCleaupAndExit("[%s] ERROR: Failed to open shared memory: %s\n", PROGRAM_NAME, strerror(errno));
     }
 
     if (ftruncate(sharedMemoryFd, sizeof(circular_buffer_data_t)) < 0) {
-        fprintf(stderr, "[%s] ERROR: Failed to truncate shared memory: %s\n", PROGRAM_NAME, strerror(errno));
-        closeSHM(NULL);
-        exit(EXIT_FAILURE);
+        printStderrCleaupAndExit("[%s] ERROR: Failed to truncate shared memory: %s\n", PROGRAM_NAME, strerror(errno));
     }
-    circular_buffer_data_t *circularBufferData;
+
     circularBufferData = mmap(NULL, sizeof(circular_buffer_data_t), PROT_READ | PROT_WRITE, MAP_SHARED, sharedMemoryFd, 0);
 
     if(circularBufferData == MAP_FAILED) {
-        fprintf(stderr, "[%s] ERROR: Failed to map shared memory: %s\n", PROGRAM_NAME, strerror(errno));
-        closeSHM(NULL);
-        exit(EXIT_FAILURE);
+        printStderrCleaupAndExit("[%s] ERROR: Failed to map shared memory: %s\n", PROGRAM_NAME, strerror(errno));
     }
 
     if(close(sharedMemoryFd) == -1) {
-        fprintf(stderr, "[%s] ERROR: Failed to close shared memory file descriptor: %s\n", PROGRAM_NAME, strerror(errno));
-        closeSHM(circularBufferData);
-        exit(EXIT_FAILURE);
+        printStderrCleaupAndExit("[%s] ERROR: Failed to close shared memory file descriptor: %s\n", PROGRAM_NAME, strerror(errno));
     }
 
     circularBufferData -> readPos = 0;
@@ -245,8 +262,6 @@ static circular_buffer_data_t* openSHM(void) {
             circularBufferData -> resultSets[i][x][1] = -1;
         }
     }
-
-    return circularBufferData;
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
@@ -258,38 +273,52 @@ static circular_buffer_data_t* openSHM(void) {
  * 
  * @param semaphoreCollection pointer to a semaphore_collection_t containing the semaphores to close
  */
-static void closeSEM(semaphore_colleciton_t* semaphoreCollection) {
-    // TODO: This has a problem: Whe one close fails it doesnt close the other ones!!!
+static int closeSEM() {
+    int returnValue = 0;
 
-    if(semaphoreCollection -> rSem != NULL) {
-        if(sem_close(semaphoreCollection -> rSem) == -1) {
-            printStderrAndExit("[%s] ERROR: Failed to close semaphores: %s\n", PROGRAM_NAME, strerror(errno));
+    if(semaphoreCollection.rSem != NULL) {
+        if(sem_close(semaphoreCollection.rSem) == -1) {
+            fprintf(stderr, "[%s] ERROR: Failed to close semaphores: %s\n", PROGRAM_NAME, strerror(errno));
+            returnValue = -1;
         }
+        semaphoreCollection.rSem = NULL;
 
         if(sem_unlink(R_SEM_NAME) == -1) {
-            printStderrAndExit("[%s] ERROR: Failed to unlink semaphores: %s\n", PROGRAM_NAME, strerror(errno));
+            fprintf(stderr, "[%s] ERROR: Failed to unlink semaphores: %s\n", PROGRAM_NAME, strerror(errno));
+            returnValue = -1;
         }
     }
+    semaphoreCollection.rSem = NULL;
 
-    if(semaphoreCollection -> wSem != NULL) {
-        if(sem_close(semaphoreCollection -> wSem) == -1) {
-            printStderrAndExit("[%s] ERROR: Failed to close semaphores: %s\n", PROGRAM_NAME, strerror(errno));
+    if(semaphoreCollection.wSem != NULL) {
+        if(sem_close(semaphoreCollection.wSem) == -1) {
+            fprintf(stderr, "[%s] ERROR: Failed to close semaphores: %s\n", PROGRAM_NAME, strerror(errno));
+            returnValue = -1;
         }
+        semaphoreCollection.wSem = NULL;
 
         if(sem_unlink(W_SEM_NAME) == -1) {
-            printStderrAndExit("[%s] ERROR: Failed to unlink semaphores: %s\n", PROGRAM_NAME, strerror(errno));
+            fprintf(stderr, "[%s] ERROR: Failed to unlink semaphores: %s\n", PROGRAM_NAME, strerror(errno));
+            returnValue = -1;
         }
     }
+    semaphoreCollection.wSem = NULL;
 
-    if(semaphoreCollection -> wSyncSem != NULL) {
-        if(sem_close(semaphoreCollection -> wSyncSem) == -1) {
-            printStderrAndExit("[%s] ERROR: Failed to close semaphores: %s\n", PROGRAM_NAME, strerror(errno));
+    if(semaphoreCollection.wSyncSem != NULL) {
+        if(sem_close(semaphoreCollection.wSyncSem) == -1) {
+            fprintf(stderr, "[%s] ERROR: Failed to close semaphores: %s\n", PROGRAM_NAME, strerror(errno));
+            returnValue = -1;
         }
+        semaphoreCollection.wSyncSem = NULL;
 
         if(sem_unlink(W_SEM_SYNC_NAME) == -1) {
-            printStderrAndExit("[%s] ERROR: Failed to unlink semaphores: %s\n", PROGRAM_NAME, strerror(errno));
+            fprintf(stderr, "[%s] ERROR: Failed to unlink semaphores: %s\n", PROGRAM_NAME, strerror(errno));
+            returnValue = -1;
         }
     }
+    semaphoreCollection.wSyncSem = NULL;
+
+    return returnValue;
 }
 
 /**
@@ -298,29 +327,18 @@ static void closeSEM(semaphore_colleciton_t* semaphoreCollection) {
  * 
  * @return A collection of the opened semaphores as a semphore_collection_t object.
  */
-static semaphore_colleciton_t openSEM(void) {
-    // TODO: Doesnt close shared memory
-    semaphore_colleciton_t semaphoreCollection = {
-        NULL,
-        NULL,
-        NULL,
-    };
-
+static void openSEM(void) {
     if((semaphoreCollection.rSem = sem_open(R_SEM_NAME, O_CREAT | O_EXCL, 0600, 0)) == SEM_FAILED) {
-        printStderrAndExit("[%s] ERROR: Failed to open semaphores: %s\n", PROGRAM_NAME, strerror(errno));
+        printStderrCleaupAndExit("[%s] ERROR: Failed to open semaphores: %s\n", PROGRAM_NAME, strerror(errno));
     }
-
+    
     if((semaphoreCollection.wSem = sem_open(W_SEM_NAME, O_CREAT | O_EXCL, 0600, MAX_NUM_RESULT_SETS)) == SEM_FAILED) {
-        closeSEM(&semaphoreCollection);
-        printStderrAndExit("[%s] ERROR: Failed to open semaphores: %s\n", PROGRAM_NAME, strerror(errno));
+        printStderrCleaupAndExit("[%s] ERROR: Failed to open semaphores: %s\n", PROGRAM_NAME, strerror(errno));
     }
 
     if((semaphoreCollection.wSyncSem = sem_open(W_SEM_SYNC_NAME, O_CREAT | O_EXCL, 0600, 1)) == SEM_FAILED) {
-        closeSEM(&semaphoreCollection);
-        printStderrAndExit("[%s] ERROR: Failed to open semaphores: %s\n", PROGRAM_NAME, strerror(errno));
+        printStderrCleaupAndExit("[%s] ERROR: Failed to open semaphores: %s\n", PROGRAM_NAME, strerror(errno));
     }
-
-    return semaphoreCollection;
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
@@ -358,25 +376,36 @@ static void registerSignalHandler(void) {
  * @param circularBufferData pointer to the mapped shared memory circular buffer
  * @param semaphoreCollection pointer to a semaphore_collection_t containing the semaphores to close
  */
-static void cleanup(circular_buffer_data_t *circularBufferData, semaphore_colleciton_t *semaphoreCollection) {
-    circularBufferData -> stopGenerators = true;
-    int semValue = 0;
-    while(semValue < MAX_NUM_RESULT_SETS) {
-        if(sem_getvalue(semaphoreCollection -> wSem, &semValue) == -1) {
-            closeSHM(circularBufferData);
-            closeSEM(semaphoreCollection);
-            printStderrAndExit("[%s] ERROR: There was an error reading the value of a semaphore: %s\n", PROGRAM_NAME, strerror(errno));
-        }
-        
-        if(sem_post(semaphoreCollection -> wSem) == -1) {
-            closeSHM(circularBufferData);
-            closeSEM(semaphoreCollection);
-            printStderrAndExit("[%s] ERROR: There was an error pushing the semaphore: %s\n", PROGRAM_NAME, strerror(errno));
+static void cleanup() {
+    bool error = false;
+    if(circularBufferData != NULL && semaphoreCollection.wSem != NULL) {
+        circularBufferData -> stopGenerators = true;
+        int semValue = 0;
+        while(semValue < MAX_NUM_RESULT_SETS) {
+            if(sem_getvalue(semaphoreCollection.wSem, &semValue) == -1) {
+                fprintf(stderr, "[%s] ERROR: There was an error reading the value of a semaphore: %s\n", PROGRAM_NAME, strerror(errno));
+                error = true;
+                break;
+            }
+            
+            if(sem_post(semaphoreCollection.wSem) == -1) {
+                fprintf(stderr, "[%s] ERROR: There was an error pushing the semaphore: %s\n", PROGRAM_NAME, strerror(errno));
+                error = true;
+                break;
+            }
         }
     }
 
-    closeSHM(circularBufferData);
-    closeSEM(semaphoreCollection);
+    if(closeSHM() == -1) {
+        error = true;
+    }
+    if(closeSEM() == -1) {
+        error = true;
+    }
+
+    if(error) {
+        exit(EXIT_FAILURE);
+    }
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
@@ -395,8 +424,8 @@ int main(int argc, char **argv) {
 
     program_parameters_t programParameters = parseArguments(argc, argv);
 
-    circular_buffer_data_t *circularBufferData = openSHM();
-    semaphore_colleciton_t semaphoreCollection = openSEM();
+    openSHM();
+    openSEM();
 
     // Wait if the delay is set
     if(programParameters.delay > 0) {
@@ -411,8 +440,8 @@ int main(int argc, char **argv) {
             if(errno == EINTR) {
                 continue;
             }
-            cleanup(circularBufferData, &semaphoreCollection);
-            printStderrAndExit("[%s] ERROR: There was an error waiting for the semaphore: %s\n", PROGRAM_NAME, strerror(errno));
+            cleanup();
+            printStderrCleaupAndExit("[%s] ERROR: There was an error waiting for the semaphore: %s\n", PROGRAM_NAME, strerror(errno));
         }
 
         int numberOfEdgesInResult = 0;
@@ -444,8 +473,8 @@ int main(int argc, char **argv) {
         circularBufferData -> readPos = circularBufferData -> readPos % MAX_NUM_RESULT_SETS;
 
         if(sem_post(semaphoreCollection.wSem) == -1) {
-            cleanup(circularBufferData, &semaphoreCollection);
-            printStderrAndExit("[%s] ERROR: There was an error pushing the semaphore: %s\n", PROGRAM_NAME, strerror(errno));
+            cleanup();
+            printStderrCleaupAndExit("[%s] ERROR: There was an error pushing the semaphore: %s\n", PROGRAM_NAME, strerror(errno));
         }
 
         ++readCounter;
@@ -457,7 +486,6 @@ int main(int argc, char **argv) {
         printf("The graph might not be 3-colorable, best solution removes %d edges.\n", numberOfEdgesInBestResult);
     }
 
-    cleanup(circularBufferData, &semaphoreCollection);
-
+    cleanup();
     return EXIT_SUCCESS;
 }
